8c06d8a8754a7076ad072bbc6fe832a151fc9869
andannn <jqn296763005@gmail.com>
2024-02-23 21:59:42 +0900

    in Process

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,60 +3,64 @@ import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import java.util.*
+import com.xenomachina.argparser.ArgParser
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 import kotlin.reflect.javaType
 import kotlin.reflect.typeOf
 
 const val RootClassName = "Test"
 val listClass = ClassName("kotlin.collections", "List")
 
+class GsonClassGeneratorArgs(parser: ArgParser) {
+    val source by parser.positional(
+        "SOURCE",
+        help = "source filename")
+
+    val destination by parser.positional(
+        "DEST",
+        help = "destination filename")
+}
+
 @OptIn(ExperimentalStdlibApi::class)
 fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
 
     val jsonString = """
-        {
-            "name": "John",
-            "age": 30,
-            "title_test": {
-                "romaji": "Cowboy Bebop",
-                "english": "Cowboy Bebop",
-                "native": "カウボーイビバップ"
-            },
-            "number_items": [1, 3, 4],
-            "image_items": [
-                {
-                    "extraLarge": "bx5-NozHwXWdNLCz.jpg",
-                    "large": "bx5-NozHwXWdNLCz.jpg"
-                },
-                {
-                    "extraLarge": "vvvvvvvvvvvvvvv.jpg",
-                    "large": "eeeeeeeeeeeeeeeee.jpg"
-                }
-            ]
-        }
+{
+  "data": {
+    "Staff": {
+      "id": 96764,
+      "image": {
+        "large": "https://s4.anilist.co/file/anilistcdn/staff/large/n96764-yJWGrhjanDJQ.png",
+        "medium": "https://s4.anilist.co/file/anilistcdn/staff/medium/n96764-yJWGrhjanDJQ.png"
+      }
+    }
+  }
+}
     """.trimIndent()
 
     val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
     val jsonAdapter: JsonAdapter<Map<String, Any>> = moshi.adapter(typeOf<Map<String, Any>>().javaType)
     val jsonObject = jsonAdapter.fromJson(jsonString) ?: throw IllegalArgumentException("Invalid JSON")
 
-    generateDataKotlinClass(className = RootClassName, jsonObject)
+    val fileBuilder = FileSpec.builder(fileName = "$RootClassName.kt", packageName = "")
+    fileBuilder.generateDataKotlinClass(className = RootClassName, jsonObject)
+
+    val output = fileBuilder.build()
+
+    println(output)
 }
 
-fun generateDataKotlinClass(className: String, jsonMap: Map<String, Any>) {
-    println("generateDataKotlinClass E $jsonMap")
+fun FileSpec.Builder.generateDataKotlinClass(className: String, jsonMap: Map<String, Any>) {
     if (jsonMap.isEmpty()) {
         println("Empty json map")
         return
     }
 
-    val fileBuilder = FileSpec.builder(fileName = "$RootClassName.kt", packageName = "")
-
     val classBuilder = TypeSpec.classBuilder(className).addModifiers(KModifier.DATA)
     val constructorBuilder = FunSpec.constructorBuilder()
     jsonMap.forEach { (propertyName, propertyValue) ->
-        val propertyNameCamel = propertyName.toLowerCamelCase()
         when (propertyValue) {
             is Map<*, *> -> {
                 generateDataKotlinClass(
@@ -65,7 +69,7 @@ fun generateDataKotlinClass(className: String, jsonMap: Map<String, Any>) {
                 )
 
                 val mapClass = ClassName(packageName = "", simpleNames = listOf(propertyName.toUpperCamelCase()))
-                (classBuilder to constructorBuilder).addPrimaryConstructor(propertyNameCamel, mapClass)
+                (classBuilder to constructorBuilder).addPrimaryConstructorAndAnnotaion(propertyName, mapClass)
             }
 
             is List<*> -> {
@@ -77,76 +81,66 @@ fun generateDataKotlinClass(className: String, jsonMap: Map<String, Any>) {
                             listClass.parameterizedBy(
                                 ClassName(packageName = "", simpleNames = listOf(propertyName.toUpperCamelCase()))
                             )
-                        (classBuilder to constructorBuilder).addPrimaryConstructor(
-                            propertyName = propertyName.toLowerCamelCase(),
+                        (classBuilder to constructorBuilder).addPrimaryConstructorAndAnnotaion(
+                            propertyName = propertyName,
                             type = listClass
                         )
                         return@forEach
                     }
 
                     val listClass = listClass.parameterizedBy(propertyValue.first()!!::class.asTypeName())
-                    (classBuilder to constructorBuilder).addPrimaryConstructor(propertyNameCamel, listClass)
+                    (classBuilder to constructorBuilder).addPrimaryConstructorAndAnnotaion(propertyName, listClass)
                 }
             }
 
             is String -> {
-                (classBuilder to constructorBuilder).addPrimaryConstructor(
-                    propertyNameCamel,
+                (classBuilder to constructorBuilder).addPrimaryConstructorAndAnnotaion(
+                    propertyName,
                     String::class.asTypeName()
                 )
             }
 
             is Int -> {
-                (classBuilder to constructorBuilder).addPrimaryConstructor(
-                    propertyNameCamel,
+                (classBuilder to constructorBuilder).addPrimaryConstructorAndAnnotaion(
+                    propertyName,
                     Int::class.asTypeName()
                 )
             }
 
             is Double -> {
-                (classBuilder to constructorBuilder).addPrimaryConstructor(
-                    propertyNameCamel,
+                (classBuilder to constructorBuilder).addPrimaryConstructorAndAnnotaion(
+                    propertyName,
                     Double::class.asTypeName()
                 )
             }
         }
     }
 
-    val output = fileBuilder.addType(
-        classBuilder.primaryConstructor(
-            constructorBuilder.build()
-        ).build()
-    ).build()
-
-    println("$output")
-}
-
-fun Pair<TypeSpec.Builder, FunSpec.Builder>.addPrimaryConstructor(propertyName: String, type: TypeName) {
-    val (classBuilder, constructorBuilder) = this
-    classBuilder.addProperty(
-        PropertySpec.builder(propertyName, type)
-            .initializer(propertyName)
+    this.addType(
+        classBuilder
+            .primaryConstructor(
+                constructorBuilder.build()
+            )
+            .addAnnotation(
+                AnnotationSpec.builder(Serializable::class).build()
+            )
             .build()
     )
-    constructorBuilder.addParameter(propertyName, type)
 }
 
-fun String.toLowerCamelCase() = let {
-    it.split("_").foldIndexed("") { index, acc, element ->
-        if (index == 0) {
-            element
-        } else {
-            acc + element.replaceFirstChar {
-                if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
-            }
-        }
-    }
+fun Pair<TypeSpec.Builder, FunSpec.Builder>.addPrimaryConstructorAndAnnotaion(propertyName: String, type: TypeName) {
+    val (classBuilder, constructorBuilder) = this
+    val propertyNameLowerCamel = propertyName.toLowerCamelCase()
+    classBuilder.addProperty(
+        PropertySpec.builder(propertyNameLowerCamel, type)
+            .initializer(propertyNameLowerCamel)
+            .addAnnotation(
+                AnnotationSpec.builder(SerialName::class)
+                    .addMember("value = %S", propertyName)
+                    .build()
+            )
+            .build()
+    )
+    constructorBuilder.addParameter(propertyNameLowerCamel, type)
 }
 
-fun String.toUpperCamelCase() = let {
-    it.split("_").foldIndexed("") { index, acc, element ->
-        acc + element.replaceFirstChar {
-            if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString()
-        }
-    }
-}
